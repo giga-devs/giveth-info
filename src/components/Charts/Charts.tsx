01a3f7a0fb503b6af4db17f544ca83a74a521281
luizapuccinelli <luizapuccinelli06@gmail.com>
2022-06-21 18:46:49 -0300

    Chart reponsive fix

--- src/components/Charts/Charts.tsx
+++ src/components/Charts/Charts.tsx
@@ -15,20 +15,21 @@ export function Charts(){
 
 const ChartsContainer = styled.div`
   display: flex;
-  width: 100%;
+  flex-direction: row;
   justify-content: space-between;
   
 	${mediaQueries.mobileS} {
 		flex-direction: column;
     gap: 50px;
+    div {
+      width: 100%;
+    }
   }
 
   ${mediaQueries.laptopL} {
 		flex-direction: row;
-
-  }
-  
-  div {
-  width: 100%;
+    div {
+      width: 99%;
+    }
   }
 `
