e30b1adaf5ed0f60b88e881a04382774ce52b0c2
luizapuccinelli <luiza.puccinelli06@gmail.com>
2022-07-17 18:01:13 -0300

    next config

--- next.config.js
+++ next.config.js
@@ -1,10 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
-
-module.exports = module.exports = {
   typescript: {
     ignoreBuildErrors: true,
   },
 }
+
+module.exports = nextConfig
