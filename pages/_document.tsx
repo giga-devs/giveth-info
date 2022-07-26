7f2cd67c8c0fe4bd11b2db48f4a4d5455b803ffe
luizapuccinelli <luiza.puccinelli06@gmail.com>
2022-07-26 11:52:28 -0300

    meta tag

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,4 +1,4 @@
-import Document, { DocumentContext } from 'next/document'
+import Document, { DocumentContext, Html, Head, Main, NextScript } from 'next/document'
 import { ServerStyleSheet } from 'styled-components'
 
 export default class MyDocument extends Document {
@@ -21,5 +21,51 @@ export default class MyDocument extends Document {
     } finally {
       sheet.seal()
     }
+  }
+
+  render(){
+    return(
+      <Html>
+        <Head>
+          <meta name="title" content="Giveth Info Dashboard" />
+          <meta
+            name="description"
+            content="Analyze the GIV token performance to understand how to help more for-good projects with zero added fees"
+          />
+
+          <meta property="og:type" content="website" />
+          <meta property="og:url" content="https://giveth-info.vercel.app/" />
+          <meta property="og:title" content="Giveth Info Dashboard" />
+          <meta
+            property="og:description"
+            content="Analyze the GIV token performance to understand how to help more for-good projects with zero added fees"
+          />
+          <meta
+            property="og:image"
+            content="https://giveth.mypinata.cloud/ipfs/QmQ9sfdevs9vS7czBXBfDaRRPhU8a6T5gXxF3NDGSnQe1c"
+          />
+
+          <meta property="twitter:card" content="summary_large_image" />
+          <meta
+            property="twitter:url"
+            content="https://giveth-info.vercel.app/"
+          />
+          <meta property="twitter:title" content="Giveth Info Dashboard" />
+          <meta
+            property="twitter:description"
+            content="Analyze the GIV token performance to understand how to help more for-good projects with zero added fees"
+          />
+          <meta
+            property="twitter:image"
+            content="https://giveth.mypinata.cloud/ipfs/QmQ9sfdevs9vS7czBXBfDaRRPhU8a6T5gXxF3NDGSnQe1c"
+          />
+          <meta name="twitter:card" content="summary_large_image" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
   }
 }
