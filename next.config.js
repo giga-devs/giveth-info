d44cf26d22d3c2b44df8962f184e0748470e949e
luizakp <luiza.puccinelli06@gmail.com>
2022-10-20 15:05:06 +0200

    upgrade next version

--- next.config.js
+++ next.config.js
@@ -6,10 +6,6 @@ const nextConfig = {
   compiler: {
     styledComponents: true,
   },
-  images: {
-    loader: 'preview-image',
-    path: 'https://giveth.mypinata.cloud/ipfs/QmQ9sfdevs9vS7czBXBfDaRRPhU8a6T5gXxF3NDGSnQe1c',
-  },
 };
 
 module.exports = nextConfig;
